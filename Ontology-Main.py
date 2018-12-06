1ef1fac1788cf8c514c1f448e8581312eb32d1ed
Adam Kona <adam.kona4@gmail.com>
2018-12-06 00:34:57 +0000

    Improved classification by boosting categories based on values found

--- Ontology-Main.py
+++ Ontology-Main.py
@@ -52,14 +52,15 @@ data = {
     'Business': ['entrepreneurship', 'opportunities', 'customer',
                  'finance', 'stock', 'exchange', 'money', 'investment', 'trader', 'bank', 'trend',
                  'interest', 'business', 'businessman', 'businesswoman', 'market', 'need', 'global', 'success',
-                 'office', 'economy', 'management', 'equity', 'competition', 'revenue', 'profit',],
+                 'office', 'economy', 'management', 'equity', 'competition', 'revenue', 'profit', ],
     'Education': ['education', 'academia', 'alumni', 'teaching', 'school', 'graduate', 'academic', 'grade', 'degree',
                   'university', 'college', 'school', 'nursery', 'homework', 'assignment', 'dissertation', 'doctorate',
                   'masters', 'phd', 'research'],
     'Engineering': ['material', 'engineering', 'environment', 'design', '3d', 'aeroacoustics', 'aerothermodynamics',
                     'air', 'turbulence', 'bridge', 'collapse', 'building', 'cad', 'car', 'fiber', 'optics',
                     'geoengineering', 'nano', 'nanotubes', 'stereo', 'tunnel'],
-    'Mathematics': ['anlge', 'measure', 'prove', 'solve', 'problem', 'equation', 'graph', 'plane', 'line', 'axis', 'algebra',
+    'Mathematics': ['anlge', 'measure', 'prove', 'solve', 'problem', 'equation', 'graph', 'plane', 'line', 'axis',
+                    'algebra',
                     'adjacent', 'coefficient', 'frequency', 'circumference', 'denominator', 'distribution', 'polygon',
                     'expression', 'factorise', 'formula', 'frequency', 'density', 'gradient', 'hcf', 'indices',
                     'integer', 'quartile', 'rational', 'irrational', 'lcm', 'average', 'mean', 'median', 'mode',
@@ -70,7 +71,7 @@ data = {
 
 }
 
-# Words -> category
+# Words -> category, makes a dictionary of keyword; category pairs
 categories = {word: key for key, words in data.items() for word in words}
 
 # Load the whole embedding matrix
@@ -82,7 +83,7 @@ with open('glove.6B.100d.txt') as f:
         embed = np.array(values[1:], dtype=np.float32)
         embeddings_index[word] = embed
 print('Loaded %s word vectors.' % len(embeddings_index))
-# Embeddings for available words
+# Embeddings for available words -  stores all known keywords embeddings
 data_embeddings = {key: value for key, value in embeddings_index.items() if key in categories.keys()}
 
 
@@ -91,31 +92,34 @@ def process(query):
     scores = {}
 
     try:
-        query_embed = embeddings_index[query]
+        query_embed = embeddings_index[query]  # gets the embeddings for the word in question
     except:
         for c in categories:
             scores[c] = 0
         return None
 
-    for word, embed in data_embeddings.items():
-        category = categories[word]
-        dist = query_embed.dot(embed)
-        dist /= len(data[category])
-        scores[category] = scores.get(category, 0) + dist
+    for word, embed in data_embeddings.items():  # iterates over the embeddings for known words
+        category = categories[word]  # gets the category of keyword
+        dist = query_embed.dot(embed)  # dot product of array of indexes for the current word and each keywords
+        dist /= len(data[category])  # divides the dot product by the number of words in that category
+        scores[category] = scores.get(category, 0) + dist  # Adds this calculated score to the current score in results
     return scores
 
 
 stop_words = set(stopwords.words('english'))
 extra_stop_words = {'lecture', 'seminar', 'talk', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday',
                     'saturday', 'sunday', 'telecastseminar', 'today', 'weh', 'adamson', 'wing', 'hall', 'baker',
-                    'rescheduled', 'am', 'pm', 'reminder', 'jan', 'feb', 'march', 'apr', 'april', 'may', 'jun', 'jul', 'aug',
-                    'sep', 'oct', 'nov', 'dec', 'spring', 'summer', 'autumn', 'winter', 'moved', 'none', 'doherty'}
+                    'rescheduled', 'am', 'pm', 'reminder', 'jan', 'feb', 'march', 'apr', 'april', 'may', 'jun', 'jul',
+                    'aug', 'sep', 'oct', 'nov', 'dec', 'spring', 'summer', 'autumn', 'winter', 'moved', 'none',
+                    'doherty', 'january', 'february', 'march', 'may', 'june', 'july', 'august', 'september', 'october'
+                                                                                                             'december'}
 
 stop_words = stop_words.union(extra_stop_words)
 
 fileid = '409'
 fileLoc = '/Users/Adam/Documents/BRUM/SecondYear/Modules/NLP/Assignment/data/untagged/' + fileid + '.txt'
 directory = '/Users/Adam/Documents/BRUM/SecondYear/Modules/NLP/Assignment/data/untagged/'
+
 topic_regx_str = r'(?:\b(?:Topic)\b:\s*)(.*)'
 special_char_regx_str = r'([^a-zA-Z ]+?)'
 
@@ -123,7 +127,6 @@ lemma = WordNetLemmatizer()
 topicRegex = re.compile(topic_regx_str)
 cleanTextRegex = re.compile(special_char_regx_str)
 
-# ps = PorterStemmer()
 pathlist = Path(directory).glob('**/*.txt')
 for path in pathlist:
     p = str(path)
@@ -138,26 +141,28 @@ for path in pathlist:
         topic = re.sub(cleanTextRegex, '', topic)
         normalized = " ".join(lemma.lemmatize(word) for word in topic.split())
         word_tokens = word_tokenize(normalized)
-        filtered_sentence = [w for w in word_tokens if not w in stop_words]
-        # filtered_sentence = [ps.stem(word) for word in filtered_sentence]
+        filtered_sentence = [w for w in word_tokens if w not in stop_words]
 
-        results = {'Computer Science': -0.5, 'Politics': -0.5, 'Biology': -0.5, 'Chemistry': -0.5, 'Physics and Astronomy': -0.5,
-                   'Languages': -0.5, 'Education': -0.5, 'Business': -0.5, 'Performing Arts': -0.5, 'Mathematics':-0.5, 'Other':0, 'Engineering': -0.5}
+        results = {'Computer Science': -0.5, 'Politics': -0.5, 'Biology': -0.5, 'Chemistry': -0.5,
+                   'Physics and Astronomy': -0.5,
+                   'Languages': -0.5, 'Education': -0.5, 'Business': -0.5, 'Performing Arts': -0.5, 'Mathematics': -0.5,
+                   'Other': 0, 'Engineering': -0.5}
 
-        # print(nltk.pos_tag(filtered_sentence))
         tagged = nltk.pos_tag(filtered_sentence)
-        filtered_sentence = [key for key, value in tagged if value == 'NN' or value == 'NNS']
-        # print
+        # filtered_sentence = [key for key, value in tagged if value == 'NN' or value == 'NNS']
         print(nltk.pos_tag(filtered_sentence))
+
         for word in filtered_sentence:
             # print('Result for %s: ' % word)
 
-            for k,v in data.items():
+            # Checks if the word is a keyword, if so then it's score gets a boost
+            for k, v in data.items():
                 if word in v:
                     current_val = results[k]
                     current_val += 10
                     results[k] = current_val
 
+            # Use word embeddings to calculate relative score to other categories
             resultStream = process(word)
             if resultStream is not None:
 
@@ -165,10 +170,9 @@ for path in pathlist:
                     current_value = results[key]
                     current_value += value
                     results[key] = current_value
+
         print("Results are as follows: ")
-        # top_two = sorted(results, key=results.get, reverse=True)[:3]
         c = Counter(results)
         most_likely = c.most_common(3)
-        print([key for key,val in most_likely])
-        # print(max(results, key=results.get))
+        print([key for key, val in most_likely])
         print()
