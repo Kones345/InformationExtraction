7781c6980012af602d5cb142df023fa6eea6152c
Adam Kona <adam.kona4@gmail.com>
2018-12-07 22:27:02 +0000

    Extracting data from the output file

--- Evaluation.py
+++ Evaluation.py
@@ -15,7 +15,26 @@ class Evaluation:
         etimes = re.findall(etime_regex_str, contents)
         sents = re.findall(sent_regex_str, contents)
         paras = re.findall(para_regex_str, contents)
-        # print(speakers)
+        print(speakers)
+        # print(locations)
+        # print(stimes)
+        # print(etimes)
+        # print(sents)
+        # print(paras)
+
+    def extractOutData(self, filename):
+        test_file_path = '/Users/Adam/Documents/BRUM/SecondYear/Modules/NLP/Assignment/out/'
+        file = open(test_file_path + filename, 'r')
+        contents = file.read()
+        speaker_regex = re.compile(knownSpeakersRegxStr)
+        speakers = speaker_regex.findall(contents)
+        location_regex = re.compile(knownLocationRegxStr)
+        locations = re.findall(location_regex, contents)
+        stimes = re.findall(stime_regex_str, contents)
+        etimes = re.findall(etime_regex_str, contents)
+        sents = re.findall(sent_regex_str, contents)
+        paras = re.findall(para_regex_str, contents)
+        print(speakers)
         # print(locations)
         # print(stimes)
         # print(etimes)
@@ -25,3 +44,4 @@ class Evaluation:
 if __name__ == '__main__':
     eval = Evaluation()
     eval.extractTestData('400.txt')
+    eval.e
